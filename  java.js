5f8521d156319c85cff96aa3be7e8d5d0c309e73
frejramstrom <frejramstrom1@gmail.com>
2023-04-10 14:27:32 +0200

    working on local storage

---  java.js
+++  java.js
@@ -1,3 +1,36 @@
+//Added "rules", they will always be referenced to the HTML//
 const ITEMS_CONTAINER = document.getElementById("items");
-const ITEM_TEMPLAATE = document.getElementById("itemTemplate");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
 const ADD_BUTTON = document.getElementById("add");
+
+//These lines fetch existing items from the localstorage.
+//To avoid null, in case if we user opens the website first time, we have or to an empty Array.
+//We need to convert JSON string into an JS array by return JSON to parse.
+
+let items = getItems();
+
+function getItems() {
+  const value = localStorage.getItem("todo") || "[]";
+
+  return JSON.parse(value);
+}
+
+//We need a function to set the items, example, added or changed an item. We need to refresh what we saved.
+
+function setItems(items) {
+  const itemsJson = JSON.stringify(items);
+
+  localStorage.setItem("todo", itemsJson);
+}
+
+//We now define the logic now to define a new object
+
+function addItem() {
+  items.unshift({
+    description: "",
+    completed: false,
+  });
+
+  setItems(items);
+  refreshList();
+}
